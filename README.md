850710c93b27e91f0b0b377041d55cbd684aa03e
upphiminn <sgr.corneliu@gmail.com>
2020-02-25 22:08:55 +0100

    feat: make it into a npm pkg and use umd pattern

--- README.md
+++ README.md
@@ -1,154 +1,183 @@
-# jDBSCAN 
+# jDBSCAN
 
----
-## Description                
+## Description
 
-**DBSCAN** is a density based clustering algorithm that works by successively growing a cluster from initial seed points **[1]**. If the density in the circle proximity (which has the radius parameter *Eps*) of a point is above or equal a threshold level, denoted by the *MinPts* parameter, the cluster is expanded forward by assigning all the unassigned points in the neighborhood to it. The algorithm then recursively proceeds with the same steps for each of the newly added points to the cluster. Points that will not be assigned to any cluster by the end of this process are labeled as noise. 
+**DBSCAN** is a density based clustering algorithm that works by successively growing a cluster from initial seed points **[1]**. If the density in the circle proximity (which has the radius parameter _Eps_) of a point is above or equal a threshold level, denoted by the _MinPts_ parameter, the cluster is expanded forward by assigning all the unassigned points in the neighborhood to it. The algorithm then recursively proceeds with the same steps for each of the newly added points to the cluster. Points that will not be assigned to any cluster by the end of this process are labeled as noise.
 
 **The current implementation supports only two dimensional data.**
 
 **[1]** http://en.wikipedia.org/wiki/DBSCAN
 
-
 ## Additional Features
 
- The algorithm also works on data points given by ***GPS coordinates***. Moreover, the algorithm can be used on data with a time dimension also. This allows for ***spatio-temporal*** clustering.   
- 
-## Usage
-1. Import the script.
+The algorithm also works on data points given by **_GPS coordinates_**. Moreover, the algorithm can be used on data with a time dimension also. This allows for **_spatio-temporal_** clustering.
 
-		<script type="text/javascript" src="jDBSCAN.js"></script>
-		
-2. Sample Data Format
+## Usage
+
+1.  Import the script.
+
+        <script type="text/javascript" src="jDBSCAN.js"></script>
+
+2.  Sample Data Format
+
 #### Basic 2D Data
+
 ```javascript
-		var point_data = [
-					{ 	
-						x: 0.1,
-						y: 5
-					}, 
-					{ 
-						x: 2,
-						y: 4
-					},
-					{ 
-						x: 0,
-						y: 7
-					}
-				];
+var point_data = [
+	{
+		x: 0.1,
+		y: 5
+	},
+	{
+		x: 2,
+		y: 4
+	},
+	{
+		x: 0,
+		y: 7
+	}
+];
 ```
+
 #### GPS Data
+
 ```javascript
-var gps_point_data = [ 
-			{ 
-				location: {
-					accuracy: 30,
-					latitude: 55.7858667,
-					longitude: 12.5233995
-				}
-			},
-			{
-				location: {
-					accuracy: 10,
-					latitude: 45.4238667,
-					longitude: 12.5233995
-				}
-			},
-		        { 	location: {
-					accuracy: 5,
-					latitude: 25.3438667,
-					longitude: 11.6533995
-				}
-			}
-		];
+var gps_point_data = [
+	{
+		location: {
+			accuracy: 30,
+			latitude: 55.7858667,
+			longitude: 12.5233995
+		}
+	},
+	{
+		location: {
+			accuracy: 10,
+			latitude: 45.4238667,
+			longitude: 12.5233995
+		}
+	},
+	{
+		location: {
+			accuracy: 5,
+			latitude: 25.3438667,
+			longitude: 11.6533995
+		}
+	}
+];
 ```
+
 Where accuracy is given in meters.
-#### Spatial and Temporal Data   
+
+#### Spatial and Temporal Data
+
 ```javascript
-		var time_gps_data = [ 
-					{ 
-						location: {
-							accuracy: 30,
-							latitude: 55.7858667,
-							longitude: 12.5233995
-						},
-						timestamp: 1349958445
-					},
-					{
-						location: {
-							accuracy: 10,
-							latitude: 45.4238667,
-							longitude: 12.5233995
-						},
-						timestamp: 123958445
-					},
-					{ 
-						location: {
-							accuracy: 5,
-							latitude: 25.3438667,
-							longitude: 11.6533995
-						},
-						timestamp: 1350958445
-					}
-				];
+var time_gps_data = [
+	{
+		location: {
+			accuracy: 30,
+			latitude: 55.7858667,
+			longitude: 12.5233995
+		},
+		timestamp: 1349958445
+	},
+	{
+		location: {
+			accuracy: 10,
+			latitude: 45.4238667,
+			longitude: 12.5233995
+		},
+		timestamp: 123958445
+	},
+	{
+		location: {
+			accuracy: 5,
+			latitude: 25.3438667,
+			longitude: 11.6533995
+		},
+		timestamp: 1350958445
+	}
+];
 ```
 
 ### OR
- 
+
 ```javascript
-		var time_point_data = [ 
-					{ 
-						x: 0.1, 
-						y: 5, 
-						timestamp: 1350958445
-					}, 
-					{
-						x: 2,
-						y: 4,
-						timestamp: 123958445
-					},
-					{
-						x: 0,
-						y: 7,	
-						timestamp: 1349958445
-					} 
-				];
+var time_point_data = [
+	{
+		x: 0.1,
+		y: 5,
+		timestamp: 1350958445
+	},
+	{
+		x: 2,
+		y: 4,
+		timestamp: 123958445
+	},
+	{
+		x: 0,
+		y: 7,
+		timestamp: 1349958445
+	}
+];
 ```
+
 Where **timestamp** is given by the **UNIX timestamp in seconds** for the sample point.
 
-3. Run the algorithm. 
-To run the algorithm you need to provide the data along with the **eps** and **minPts** parameters. For the traditional **DBSCAN** the steps are the following: 
+3. Run the algorithm. To run the algorithm you need to provide the data along with the **eps** and **minPts** parameters. For the traditional **DBSCAN** the steps are the following:
+
 ```javascript
-	// Configure a DBSCAN instance.
-	var dbscanner = jDBSCAN().eps(0.075).minPts(1).distance('EUCLIDEAN').data(point_data);
+// Configure a DBSCAN instance.
+var dbscanner = jDBSCAN()
+	.eps(0.075)
+	.minPts(1)
+	.distance('EUCLIDEAN')
+	.data(point_data);
 ```
+
 The distance functions available are: **'EUCLIDEAN', 'HAVERSINE'** (for GPS data), **'MANHATTAN'**.
 
-Additionally you can provide your own distance function, which must accept at least two parameters (the two points), and passing it to the *distance* method. The next step is to simply run the clustering algorithm.
+Additionally you can provide your own distance function, which must accept at least two parameters (the two points), and passing it to the _distance_ method. The next step is to simply run the clustering algorithm.
 
 ```javascript
-	// This will return the assignment of each point to a cluster number, 
+	// This will return the assignment of each point to a cluster number,
 	// points which have  -1 as assigned cluster number are noise.
 	var point_assignment_result = dbscanner();
-		
+
 	// (OPTIONAL) If you need the cluster centers for each of the
-	// identified clusters use this 
+	// identified clusters use this
 	var cluster_centers = dbscanner.getClusters();  `
 ```
 
 In case of **spatio-temporal data**, as described above, **additional parameters must be supplied**. Such as **time_eps** (difference in seconds used as the time equivalent of the distance eps value).
 
 ```javascript
-	var dbscanner = jDBSCAN().eps(0.075).minPts(1).distance('EUCLIDEAN').timeEps(1800).data(data);
+var dbscanner = jDBSCAN()
+	.eps(0.075)
+	.minPts(1)
+	.distance('EUCLIDEAN')
+	.timeEps(1800)
+	.data(data);
 ```
-The default **time distance function** is given by the absolute difference between timestamps. Other functions can be used by passing a function to the *time_distance* method, it also should accept two objects with a *timestamp* field.
+
+The default **time distance function** is given by the absolute difference between timestamps. Other functions can be used by passing a function to the _time_distance_ method, it also should accept two objects with a _timestamp_ field.
+
 ```javascript
-	var dbscanner = jDBSCAN().eps(0.075).minPts(1).distance('EUCLIDEAN').timeEps(1800).timeDistance(custom_function).data(data);
+var dbscanner = jDBSCAN()
+	.eps(0.075)
+	.minPts(1)
+	.distance('EUCLIDEAN')
+	.timeEps(1800)
+	.timeDistance(custom_function)
+	.data(data);
 ```
+
 The remaining steps are similar.
 
 ## Example
-See **example.html**, use the console to view the raw input data and raw output.
-![](example/default.png)
+
+See **example.html**, use the console to view the raw input data and raw output. ![](example/default.png)
+
 #### After DBSCAN
+
 ![](example/clusters.png)
---
+++ src/jDBScan.js
@@ -0,0 +1,313 @@
+/* 
+ Author: Corneliu S. (github.com/upphiminn)
+ 2013
+ */
+
+const jDBSCAN = function() {
+	// Local instance vars.
+	let eps;
+	let time_eps;
+	let minPts;
+	let data = [];
+	let clusters = [];
+	let status = [];
+	let graph = [];
+	let distance = euclidean_distance;
+	let time_distance = timestamp_distance;
+
+	// Utils
+	function array_min(array, f) {
+		let i = -1;
+		let n = array.length;
+		let a;
+		let b;
+
+		if (arguments.length === 1) {
+			while (++i < n && !((a = array[i]) != null && a <= a)) a = undefined;
+			while (++i < n) if ((b = array[i]) != null && a > b) a = b;
+		} else {
+			while (++i < n && !((a = f.call(array, array[i], i)) != null && a <= a)) a = undefined;
+			while (++i < n) if ((b = f.call(array, array[i], i)) != null && a > b) a = b;
+		}
+
+		return a;
+	}
+
+	function array_max(array, f) {
+		let i = -1;
+		let n = array.length;
+		let a;
+		let b;
+
+		if (arguments.length === 1) {
+			while (++i < n && !((a = array[i]) != null && a <= a)) a = undefined;
+			while (++i < n) if ((b = array[i]) != null && b > a) a = b;
+		} else {
+			while (++i < n && !((a = f.call(array, array[i], i)) != null && a <= a)) a = undefined;
+			while (++i < n) if ((b = f.call(array, array[i], i)) != null && b > a) a = b;
+		}
+
+		return a;
+	}
+
+	// Distance Functions
+	function timestamp_distance(point1, point2) {
+		return Math.abs(point2.timestamp - point1.timestamp);
+	}
+
+	function euclidean_distance(point1, point2) {
+		return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
+	}
+
+	function manhattan_distance(point1, point2) {
+		return Math.abs(point2.x - point1.x) + Math.abs(point2.y - point1.y);
+	}
+
+	function haversine_distance(point1, point2) {
+		// default 4 sig figs reflects typical 0.3% accuracy of spherical model
+		if (typeof precision === 'undefined') {
+			let precision = 4;
+		}
+
+		const R = 6371;
+		const lat1 = (point1.location.latitude * Math.PI) / 180;
+		const lon1 = (point1.location.longitude * Math.PI) / 180;
+		const lat2 = (point2.location.latitude * Math.PI) / 180;
+		const lon2 = (point2.location.longitude * Math.PI) / 180;
+
+		const dLat = lat2 - lat1;
+		const dLon = lon2 - lon1;
+
+		const a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		const d = R * c;
+
+		return d.toPrecision(precision);
+	}
+
+	// Core Algorithm Related
+	function get_region_neighbours(point_idx) {
+		const neighbours = [];
+		let d = data[point_idx];
+
+		for (let i = 0; i < data.length; i++) {
+			if (point_idx === i) {
+				continue;
+			}
+
+			if (time_eps && time_distance(data[i], d) <= time_eps && distance(data[i], d) <= eps) {
+				neighbours.push(i);
+			} else if (distance(data[i], d) <= eps) {
+				neighbours.push(i);
+			}
+		}
+
+		return neighbours;
+	}
+
+	function expand_cluster(point_idx, neighbours, cluster_idx) {
+		clusters[cluster_idx - 1].push(point_idx); // add point to cluster
+		status[point_idx] = cluster_idx; // assign cluster id
+
+		for (let i = 0; i < neighbours.length; i++) {
+			const curr_point_idx = neighbours[i];
+
+			if (status[curr_point_idx] === undefined) {
+				status[curr_point_idx] = 0; // visited and marked as noise by default
+				let curr_neighbours = get_region_neighbours(curr_point_idx);
+				let curr_num_neighbours = curr_neighbours.length;
+
+				if (curr_num_neighbours >= minPts) {
+					expand_cluster(curr_point_idx, curr_neighbours, cluster_idx);
+				}
+			}
+
+			if (status[curr_point_idx] < 1) {
+				// not assigned to a cluster but visited (= 0)
+				status[curr_point_idx] = cluster_idx;
+				clusters[cluster_idx - 1].push(curr_point_idx);
+			}
+		}
+	}
+
+	let dbscan = function() {
+		status = [];
+		clusters = [];
+
+		for (let i = 0; i < data.length; i++) {
+			if (status[i] === undefined) {
+				status[i] = 0; // visited and marked as noise by default
+				const neighbours = get_region_neighbours(i);
+				const num_neighbours = neighbours.length;
+
+				if (num_neighbours < minPts) {
+					status[i] = 0; // noise
+				} else {
+					clusters.push([]); // empty new cluster
+					const cluster_idx = clusters.length;
+					expand_cluster(i, neighbours, cluster_idx);
+				}
+			}
+		}
+
+		return status;
+	};
+
+	// Resulting Clusters Center Points
+	dbscan.getClusters = function() {
+		const num_clusters = clusters.length;
+		const clusters_centers = [];
+
+		for (let i = 0; i < num_clusters; i++) {
+			if (distance === euclidean_distance || distance === manhattan_distance) {
+				clusters_centers[i] = { x: 0, y: 0 };
+
+				for (let j = 0; j < clusters[i].length; j++) {
+					clusters_centers[i].x += data[clusters[i][j]].x;
+					clusters_centers[i].y += data[clusters[i][j]].y;
+				}
+
+				clusters_centers[i].x /= clusters[i].length;
+				clusters_centers[i].y /= clusters[i].length;
+				clusters_centers[i].dimension = clusters[i].length;
+				clusters_centers[i].parts = clusters[i];
+			}
+
+			if (distance === haversine_distance) {
+				clusters_centers[i] = { location: { latitude: 0, longitude: 0, accuracy: 0 } };
+
+				for (let j = 0; j < clusters[i].length; j++) {
+					clusters_centers[i].location.latitude += data[clusters[i][j]].location.latitude;
+					clusters_centers[i].location.longitude += data[clusters[i][j]].location.longitude;
+					clusters_centers[i].location.accuracy += data[clusters[i][j]].location.accuracy;
+				}
+
+				clusters_centers[i].dimension = clusters[i].length;
+				clusters_centers[i].parts = clusters[i];
+				clusters_centers[i].location.latitude /= clusters[i].length;
+				clusters_centers[i].location.longitude /= clusters[i].length;
+				clusters_centers[i].location.accuracy /= clusters[i].length;
+			}
+
+			if (time_eps) {
+				clusters_centers[i].duration = {};
+				clusters_centers[i].duration.start = array_min(clusters[i], function(d) {
+					return data[d].timestamp;
+				});
+				clusters_centers[i].duration.end = array_max(clusters[i], function(d) {
+					return data[d].timestamp;
+				});
+				clusters_centers[i].duration.span =
+					clusters_centers[i].duration.end - clusters_centers[i].duration.start;
+			}
+		}
+
+		return clusters_centers;
+	};
+
+	// Getters and setters
+	dbscan.data = function(d) {
+		if (arguments.length === 0) {
+			return data;
+		}
+
+		if (Array.isArray(d)) {
+			data = d;
+		}
+
+		return dbscan;
+	};
+
+	dbscan.eps = function(e) {
+		if (arguments.length === 0) {
+			return eps;
+		}
+
+		if (typeof e === 'number') {
+			eps = e;
+		}
+
+		return dbscan;
+	};
+
+	dbscan.timeEps = function(e) {
+		if (arguments.length === 0) {
+			return time_eps;
+		}
+
+		if (typeof e === 'number') {
+			time_eps = e;
+		}
+
+		return dbscan;
+	};
+
+	dbscan.minPts = function(p) {
+		if (arguments.length === 0) {
+			return minPts;
+		}
+
+		if (typeof p === 'number') {
+			minPts = p;
+		}
+
+		return dbscan;
+	};
+
+	dbscan.distance = function(fn) {
+		if (arguments.length === 1) {
+			if (typeof fn === 'string') {
+				switch (fn) {
+					case 'HAVERSINE':
+						distance = haversine_distance;
+						break;
+					case 'EUCLIDEAN':
+						distance = euclidean_distance;
+						break;
+					case 'MANHATTAN':
+						distance = manhattan_distance;
+						break;
+					default:
+						distance = euclidean_distance;
+				}
+			} else if (typeof fn === 'function') {
+				distance = fn;
+			}
+		}
+
+		return dbscan;
+	};
+
+	dbscan.timeDistance = function(fn) {
+		if (arguments.length === 1) {
+			time_distance = fn;
+		}
+
+		return dbscan;
+	};
+
+	return dbscan;
+};
+
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.jDBSCAN = factory();
+	}
+})(typeof self !== 'undefined' ? self : this, function() {
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return jDBSCAN;
+});
